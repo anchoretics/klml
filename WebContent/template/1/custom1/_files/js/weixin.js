5c6d68df66b3af9301a592d5d0bbff93cd8a518b
anchoretic <anchoretic@vip.qq.com>
2015-06-29 19:40:26 +0800

    修改js合法性

--- WebContent/template/1/custom1/_files/js/weixin.js
+++ WebContent/template/1/custom1/_files/js/weixin.js
@@ -11,17 +11,17 @@ $(document).ready(function(e) {
 	b();
 	$('#gotop').click(function(){
 		$(document).scrollTop(0);	
-	})
+	});
 	$('#code').hover(function(){
 			$(this).attr('id','code_hover');
 			$('#code_img').show();
 		},function(){
 			$(this).attr('id','code');
 			$('#code_img').hide();
-	})
+	});
 	
 });
 
 $(window).scroll(function(e){
 	b();		
-})
+});
